20dc5cca5ddf27131120bcbc66c62d976aa9fd89
Kino <faiza.fehri+github@gmail.com>
2024-01-31 12:04:42 +0100

    perf: path fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
         ],
       },
       {
-        path: "/user",
+        path: "/profil",
         children: [
           {
             path: "inscription",
